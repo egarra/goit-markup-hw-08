2d9922772d21f1d939abebcd0b0a35118b36b876
egarra <garikhabuzov@gmail.com>
2022-09-23 15:59:26 +0300

    hw4_inProgress

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import modal from "./modules/modal.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    modal('.header-btn', '.overflow', '.modal', '.close-btn', '.close-btn-svg');
+});
+
---
+++ js/modules/modal.js
@@ -0,0 +1,47 @@
+const modal = (trigger, overflow, closeTrigger, icon) => {
+    const triggerBtn = document.querySelector(trigger),
+          overflowWindow = document.querySelector(overflow),
+          closeBtn = document.querySelector(closeTrigger),
+          iconSvg = document.querySelector(icon),
+          scroll = caclScroll();
+
+
+    overflowWindow.style.display = 'none';
+
+    triggerBtn.addEventListener('click', () => {
+        overflowWindow.style.display = 'flex';
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+    });
+
+    closeBtn.addEventListener('click', (e) => {
+        if(e.target === closeBtn || e.target === iconSvg) {
+            overflowWindow.style.display = 'none';
+            document.body.style.overflow = "";
+            document.body.style.marginRight = `0px`;
+        }
+    });
+
+    overflowWindow.addEventListener('click', () => {
+        overflowWindow.style.display = 'none';
+        document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
+    });
+
+    function caclScroll () {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+}
+
+export default modal;
